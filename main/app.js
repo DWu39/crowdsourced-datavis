96c3d23b5f642774b3ababe391a53a2a56aafc3a
DWu39 <dwu399@gmail.com>
2015-03-23 17:56:17 -0400

    latest version but broken

--- main/app.js
+++ main/app.js
@@ -22,6 +22,37 @@ io.sockets.on('connection', function (socket) {
 
 });
 
+var users = {}; // hold users connect to chat
+
+io.on('connection', function(socket){
+
+    // Connect event
+    socket.on('join', function(name) {
+        users[socket.id] = name;
+        console.log('connection');
+        // TODO: get user name and send to chart.html
+        socket.broadcast.emit('new connection', {
+            name : users[socket.id],
+            msg: users[socket.id] + ' connected'
+        });
+    });
+
+    // Disconnect event
+    socket.on('disconnect', function(){
+        delete users[socket.id];
+        socket.broadcast.emit('disconnection', 'a user disconnected');
+    });
+
+    // Chatting event
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+        // TODO: send message with username to chart.html
+        io.emit('chat message', {
+            msg: msg,
+            user: users[socket.id]
+        });
+    });
+});
 
 
 // ----------- Initialization ----------- //
